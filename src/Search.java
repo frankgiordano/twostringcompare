1b387ef29a92256195d06467ba6e3f1dbec84e47
frankgiordano <giofr01@yahoo.com>
2016-03-05 15:54:27 -0500

    overflow check in binary search and corner cases checks

--- src/Search.java
+++ src/Search.java
@@ -3,6 +3,10 @@ public class Search {
 	
 	public static boolean sequentialSearch(String sourceString, String searchString) {
 		
+		if (sourceString == null || sourceString.equals("") || searchString == null || searchString.equals("")) {
+			return false;
+		}
+		
 		int foundcount = 0;
 		
 		for (int i = 0; i < searchString.length(); i++) {
@@ -28,10 +32,14 @@ public class Search {
 		int low = 0;
 		int middle = 0;
 		int high = sourceString.length() - 1;
+		
+		if (sourceString == null || sourceString.equals("")) {
+			return false;
+		}
 	
 		while (low <= high) {
 		
-			middle = (low + high) / 2;
+			middle = low + ((high - low) / 2);
 			if (sourceString.charAt(middle) > find) {
 				high = middle - 1;
 			} else if (sourceString.charAt(middle) < find) {
