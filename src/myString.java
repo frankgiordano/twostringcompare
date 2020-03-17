1a19274d9e3fec6fd8b9c7f7892fb780213a7feb
Frank Giordano <giofr01@yahoo.com>
2020-03-17 12:24:46 -0400

    reformat

--- src/myString.java
+++ src/myString.java
@@ -1,30 +1,27 @@
 
 public class myString {
-	
-	
-	public static String removeDuplicates (String text) {
-		
-		StringBuilder endResult = new StringBuilder();
-		
-		char currentC;
-		boolean flag = false;
-		
-		for (int i = 0; i < text.length(); i++) {
-			currentC = text.charAt(i);
-			flag = false;
-			for (int y = 0; y < i; y++)
-				if (currentC == text.charAt(y)) {
-					flag = true;
-					break;
-				}
 
-			if (!flag)
-				endResult.append(currentC);
-		
-		}
-		
-		// System.out.println("result with removed dups: " + endResult);
-		return endResult.toString();
-	}
-	
+    public static String removeDuplicates(String text) {
+
+        StringBuilder endResult = new StringBuilder();
+
+        char currentC;
+        boolean flag = false;
+        for (int i = 0; i < text.length(); i++) {
+            currentC = text.charAt(i);
+            flag = false;
+            for (int y = 0; y < i; y++)
+                if (currentC == text.charAt(y)) {
+                    flag = true;
+                    break;
+                }
+
+            if (!flag)
+                endResult.append(currentC);
+        }
+
+        // System.out.println("result with removed dups: " + endResult);
+        return endResult.toString();
+    }
+
 }
